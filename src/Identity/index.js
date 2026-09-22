cebc929d8275c05ea4d40c29328af154b9ac34d6
restforeverything2025-eng <rest.for.everything.2025@gmail.com>
2026-09-22 16:00:51 +0300

    Export Telegram OIDC verifier

--- src/Identity/index.js
+++ src/Identity/index.js
@@ -1,11 +1,8 @@
 /**
- * ============================================================
- * PREVIA Core
- * Identity
- * Public API
- * ============================================================
+ * PREVIA Core - Identity Public API
  */
 
 export * from "./IdentityProvider.js";
 export * from "./TelegramIdentityVerifier.js";
 export * from "./TelegramLoginVerifier.js";
+export * from "./TelegramOidcVerifier.js";
